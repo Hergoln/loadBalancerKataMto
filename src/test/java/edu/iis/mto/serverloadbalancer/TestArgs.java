765330bdf28ebe2fce6c06b4dd39f87e1b5b3710
Hergoln <juliusz.malka@o2.pl>
2020-06-02 17:07:22 +0200

    dodano klasę TestArgs

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/TestArgs.java
@@ -0,0 +1,11 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class TestArgs {
+    public int serverCapacity;
+    public int vmsSize;
+    public double loadPercentage;
+
+    public TestArgs() {
+
+    }
+}
