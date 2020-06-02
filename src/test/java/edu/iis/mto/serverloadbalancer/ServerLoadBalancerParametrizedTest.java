c738a82f4fa7db9433495382d9544fa8e56068b1
Hergoln <juliusz.malka@o2.pl>
2020-06-02 17:38:17 +0200

    zmiana balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm na test parametryczny

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -1,22 +1,26 @@
 package edu.iis.mto.serverloadbalancer;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.Test;
+public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest {
 
-public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
-	@Test
-	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
-		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+    @ParameterizedTest(name = "{index} [{arguments}]")
+    @CsvSource({"1:1:100.0", "2:1:50.0", "4:1:25.0", "10:5:50.0", "20:15:75.0"})
+    public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm(@ConvertWith(ServerAndVmsConverter.class) TestArgs args) {
+        Server theServer = a(server().withCapacity(args.serverCapacity));
+        Vm theVm = a(vm().ofSize(args.vmsSize));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
-		assertThat(theServer, hasLoadPercentageOf(100.0d));
-		assertThat("the server should contain vm", theServer.contains(theVm));
-	}
-	
-	
+        assertThat(theServer, hasLoadPercentageOf(args.loadPercentage));
+        assertThat("the server should contain vm", theServer.contains(theVm));
+    }
+
+
 }
